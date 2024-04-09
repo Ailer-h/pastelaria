6ede8f53e85bb730c863802f327cf29602e600d6
Ailer-h <henriqueailer2@gmail.com>
2024-04-09 17:10:20 -0300

    backed up the database

--- pastelaria.sql
+++ pastelaria.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 09, 2024 at 11:49 AM
--- Server version: 8.0.31
--- PHP Version: 8.0.26
+-- Generation Time: Apr 09, 2024 at 08:08 PM
+-- Server version: 8.0.18
+-- PHP Version: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -29,13 +30,13 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `estoque`;
 CREATE TABLE IF NOT EXISTS `estoque` (
-  `id_item` int NOT NULL AUTO_INCREMENT,
+  `id_item` int(11) NOT NULL AUTO_INCREMENT,
   `nome_item` varchar(50) NOT NULL,
   `data_vencimento` date NOT NULL,
   `valor_custo` decimal(10,2) NOT NULL,
   `unidade_medida` varchar(2) NOT NULL,
-  `qtd` int NOT NULL,
-  `qtd_padrao` int NOT NULL,
+  `qtd` int(11) NOT NULL,
+  `qtd_padrao` int(11) NOT NULL,
   PRIMARY KEY (`id_item`)
 ) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -59,10 +60,12 @@ INSERT INTO `estoque` (`id_item`, `nome_item`, `data_vencimento`, `valor_custo`,
 
 DROP TABLE IF EXISTS `usuarios`;
 CREATE TABLE IF NOT EXISTS `usuarios` (
-  `id_user` int NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
   `nome_user` varchar(50) NOT NULL,
   `email_user` varchar(50) NOT NULL,
   `senha_user` varchar(50) NOT NULL,
+  `cpf_user` varchar(14) NOT NULL,
+  `cargo_user` varchar(50) NOT NULL,
   `tipo_user` char(1) NOT NULL,
   PRIMARY KEY (`id_user`)
 ) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -71,12 +74,12 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 -- Dumping data for table `usuarios`
 --
 
-INSERT INTO `usuarios` (`id_user`, `nome_user`, `email_user`, `senha_user`, `tipo_user`) VALUES
-(1, 'Henrique Ailer', 'ailer@email.com', 'senha-padrao', 'a'),
-(2, 'Caio Paulena', 'caio@email.com', 'senha-padrao', 'a'),
-(3, 'João Silva', 'joao@email.com', 'senha-padrao', 'f'),
-(4, 'Arthur Souza', 'arthur@email.com', 'senha-padrao', 'f'),
-(5, 'Admnistrador', 'adm', 'adm', 'a');
+INSERT INTO `usuarios` (`id_user`, `nome_user`, `email_user`, `senha_user`, `cpf_user`, `cargo_user`, `tipo_user`) VALUES
+(1, 'Henrique Ailer', 'ailer@email.com', 'senha-padrao', '', '', 'a'),
+(2, 'Caio Paulena', 'caio@email.com', 'senha-padrao', '', '', 'a'),
+(3, 'João Silva', 'joao@email.com', 'senha-padrao', '', '', 'f'),
+(4, 'Arthur Souza', 'arthur@email.com', 'senha-padrao', '', '', 'f'),
+(5, 'Admnistrador', 'adm', 'adm', '', '', 'd');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
