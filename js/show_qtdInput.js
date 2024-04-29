d86b0340afbb6bc609aefeaa391361aa6b0de3d6
Ailer-h <henriqueailer2@gmail.com>
2024-04-29 15:43:02 -0300

    Started working on image submisson to the database. There's still debugging to do

--- js/show_qtdInput.js
+++ js/show_qtdInput.js
@@ -5,11 +5,15 @@ function showInput(input_id, p_id){
 
     if(input.style.opacity == 0){
         input.style.opacity = 1;
+        input.required = true;
+        input.disabled = false;
         p.style.opacity = 1;
         return;
     }
 
     input.style.opacity = 0;
+    input.required = false;
+    input.disabled = true;
     p.style.opacity = 0;
 
 }
--- php/tabelaProdutos.php
+++ php/tabelaProdutos.php
@@ -6,6 +6,50 @@
     //Checa se o usuário tem permissões para entrar na pagina
     include "utilities/checkPermissions.php";
 
+    if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['cadastrar'])){
+
+        echo"<script>console.log('wow')</script>";
+
+        $nome = $_POST['nome'];
+        $val_venda = $_POST['val_venda'];
+        $img = $_FILES['image'];
+
+        if($img != NULL){
+            $temp_name = $img['tmp_name'];
+            $file_name = $img['name'];
+            
+            $pic = base64_encode(file_get_contents(addslashes($img['tmp_name'])));
+
+
+            include "utilities/mysql_connect.php";
+
+            $id_query = mysqli_query($connection, "select id_item, valor_custo, unidade_medida from estoque;");
+            $ingredients = [];
+            
+
+            while($output = mysqli_fetch_array($id_query)){
+
+                if(isset($_POST['qtd'.$output[0]])){
+
+                    echo"<script>console.log('wow')</script>";
+
+                    $qtd = $_POST['qtd'.$output[0]];
+
+                    $query = mysqli_query($connection, "insert into produtos(nome_prod, img_prod, id_ingrediente, preco_custo, qtd_ingrediente, valor_venda) values('$nome','$pic','$output[0]','$output[1]','$qtd','$val_venda')");
+
+                }
+
+            }
+
+            unlink($temp_name);
+            mysqli_close($connection);
+
+            // header("Location: tabelaProdutos.php");
+        
+        }
+
+    }
+
     function getProducts(){
         
         $qtd_inputs = 0;
@@ -17,12 +61,64 @@
             echo"<label for='check$output[0]'>$output[1] (R$$output[3]/$$output[2])</label>";
             echo"<input type='checkbox' name='check$output[0]' id='check$output[0]' onchange='showInput(\"qtd$output[0]\", \"p$output[0]\")'>";
             echo"<div style='display: flex; gap: .4em;'>
-                    <input type='number' name='qtd$output[0]' id='qtd$output[0]' style='opacity: 0; width: 5em;'>
+                    <input type='number' name='qtd$output[0]' id='qtd$output[0]' style='opacity: 0; width: 5em;' disabled>
                     <p id='p$output[0]' style='opacity: 0;'>$output[2]</p>
                 </div>";
         }
 
         mysqli_close($connection);
+
+    }
+
+    function setForm($form_id){
+
+        if($form_id == 0){
+            echo"<div class='center-absolute'>
+            <div class='header'>
+                <h1 id='titulo-form'>Novo Produto</h1>
+                <img src='../images/icons/close.png' id='close-register' onclick='location.href = location.href'>
+            </div>
+            <form action='tabelaProdutos.php' method='post' enctype='multipart/form-data'>
+            <div class='form-holder'>
+                <div class='half-1'>
+                    <div class='r-one'>
+                        <div style='display: flex; flex-direction: column;'>
+                            <label for='nome'>Nome:</label>
+                            <input type='text' name='nome' id='nome' required>
+                        </div>
+                        
+                        <div style='display: flex; flex-direction: column;'>
+                            <label for='val_venda'>Valor de Venda:</label>
+                            <input type='number' name='val_venda' id='val_venda' min='0.0001' step='any' required>
+                        </div>
+
+                    </div>
+
+                    <div class='r-two'>
+
+                            <div class='prod-grid'>";
+
+                                    getProducts();
+                            
+                            echo"</div>
+
+                    </div>
+
+                    <input type='submit' name='cadastrar' value='Cadastrar'>
+                </div>
+                <div class='half-2'>
+                    <div class='img-frame'><img class='img-thumbnail' id='img-thumbnail'></div>
+                    <p id='img-filename' style='font-style: italic;'></p>
+                    <div class='img-input'>
+                        <label for='img' class='label'>Imagem do Produto</label>
+                        <input type='file' name='image' id='img' accept='.jpg, .jpeg, .png' onchange='changePlaceholder(\"img-filename\", this.id, \"img-thumbnail\")' required>
+                    </div>
+                </div>
+
+                </div>
+            </form>
+        </div>";
+        }
 
     }
 
@@ -69,15 +165,15 @@
     <div class="center">
         <div class="table-header">
 
-            <form action="tabelaFuncionarios.php" method="post" id="searchbox">
+            <form action="tabelaProdutos.php" method="post" id="searchbox">
                 <input type="text" placeholder="Pesquisar..." name="search" id="searchbar">
                 <input type="submit" value="🔎︎">
-                <a href="tabelaFuncionarios.php"><img src="../images/icons/close.png" id="close-search"></a>
+                <a href="tabelaProdutos.php"><img src="../images/icons/close.png" id="close-search"></a>
             </form>
 
             <div id="menu">
                 <button onclick=""><img src="../images/icons/report.png"></button>
-                <form action="tabelaFuncionarios.php" method="post">
+                <form action="tabelaProdutos.php" method="post">
                     <input type="hidden" name="new-item" value="0">
                     <button><img src="../images/icons/plus.png"></button>
                 </form>
@@ -98,53 +194,14 @@
     </div>
 
     <div id="form-box">
-    <div class='center-absolute'>
-                <div class='header'>
-                    <h1 id='titulo-form'>Novo Produto</h1>
-                    <img src='../images/icons/close.png' id='close-register' onclick='location.href = location.href'>
-                </div>
-                <form action='tabelaProdutos.php' method='post'>
-                <div class='form-holder'>
-                    <div class='half-1'>
-                        <div class='r-one'>
-                            <div style="display: flex; flex-direction: column;">
-                                <label for="nome">Nome:</label>
-                                <input type="text" name="nome" id="nome">
-                            </div>
-                            
-                            <div style="display: flex; flex-direction: column;">
-                                <label for="val_venda">Valor de Venda:</label>
-                                <input type="text">
-                            </div>
-
-                        </div>
-
-                        <div class='r-two'>
-
-                                <div class="prod-grid">
-
-                                    <?php
-                                        getProducts();
-                                    ?>
-                                
-                                </div>
-
-                        </div>
-
-                        <input type="submit" value="Cadastrar">
-                    </div>
-                    <div class='half-2'>
-                        <div class='img-frame'><img class='img-thumbnail' id='img-thumbnail'></div>
-                        <p id='img-filename' style='font-style: italic;'></p>
-                        <div class='img-input'>
-                            <label for='img' class='label'>Imagem do Produto</label>
-                            <input type='file' name='img' id='img' accept='image/*' onchange='changePlaceholder("img-filename", this.id, "img-thumbnail")'>
-                        </div>
-                    </div>
-
-                    </div>
-                </form>
-            </div>
+        <?php
+        
+        if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['new-item'])){
+            setForm(0);
+        
+        }
+        
+        ?>
     </div>
 
 </body>
